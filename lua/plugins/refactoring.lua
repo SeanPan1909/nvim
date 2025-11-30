f9c24198706e5b8e6ec977e9158d8d22f954066d
Sean Pan <seanp1909@hotmail.com>
2025-11-30 15:10:29 +0000

    Refactor refacotring.lua

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -5,5 +5,20 @@ return {
     "nvim-treesitter/nvim-treesitter",
   },
   lazy = false,
-  opts = {},
+  config = function()
+    local refactoring = require("refactoring")
+
+    -- Setup with Go prompts enabled (prompts only when types can't be inferred)
+    refactoring.setup({
+      prompt_func_param_type = {
+        go = true,
+      },
+      prompt_func_return_type = {
+        go = true,
+      },
+      -- Show messages when extracting
+      show_success_message = true,
+    })
+    return nil
+  end,
 }
